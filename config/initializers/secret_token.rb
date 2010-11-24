8a36d97940e459cf95fd77f5eede9b5f5fe7fc4b
Vladimir Krylov <s6numid@gmail.com>
2010-11-24 16:57:43 +0200

    creation of an application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BeFeet::Application.config.secret_token = 'dc76d0ad391c24be46435488658fbb44af583450f633d0a0a8e6d013607c4b2146f43f8cef24971538ce8140939b6109976e5a38e85fe2d8962edc03be01574a'
