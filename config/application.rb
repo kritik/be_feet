7e2a4250569a197fc49a60923e8a9d71625349c5
Vladimir Krylov <s6numid@gmail.com>
2010-11-26 17:42:38 +0200

    rails.js was added

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,8 @@ module BeFeet
     # config.i18n.default_locale = :de
 
     # JavaScript files you want as :defaults (application.js is always included).
-    config.action_view.javascript_expansions[:defaults] = %w()
+    config.action_view.javascript_expansions[:defaults] = %w(jquery jquery-ui rails application)
+
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
